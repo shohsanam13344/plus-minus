a13daadb07b24c3b8e962186a036c2cd4508426e
shohsanam13344 <87891076+shohsanam13344@users.noreply.github.com>
2022-10-02 13:49:14 +0500

    dars-6 tugadi

--- src/Companent/Task.jsx
+++ src/Companent/Task.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Container,Boxs, Box1, Box2, Title, Input,Input1,Div,Div1,P,Button,P1} from './style'
+import {Container,Boxs, Box1, Box2, Title, Input,Input1,Div,Div1,P,Button,P1,P2} from './style'
 
 class Task extends Component {
   render() {
@@ -9,15 +9,13 @@ class Task extends Component {
              <Box1>
                 <Div>
                        <Title>Login I Register</Title>
-                       <P>Enter your email and password to register.</P>
-                       {/* <Title>Register</Title> */}
+                       <P>Enter your username and password to login.</P>
                      <Div1>
-                         <Input type="text" placeholder='Username' />
-                         <Input type="email" placeholder='Enter your email address' />
-                         <Input type="password" placeholder='Password' />
-                         <Input type="password" placeholder='Confirm Password' />
+                         <Input type="email" placeholder='almamun_uxui@outlook.com' />
+                         <Input type="possword" placeholder='...' />
+                         <P2>Forgot Password?</P2>
                     </Div1>
-                    <Button>Register</Button>
+                    <Button>Login</Button>
                     <P1>Or register with</P1>
                     <Input1 type="text" placeholder='Continue with Google' />
                     <Input1 type="text" placeholder='Continue with Facebook' />
@@ -27,7 +25,6 @@ class Task extends Component {
              <Div>
                        <Title>Login I Register</Title>
                        <P>Enter your email and password to register.</P>
-                       {/* <Title>Register</Title> */}
                      <Div1>
                          <Input type="text" placeholder='Username' />
                          <Input type="email" placeholder='Enter your email address' />
--- src/Companent/style.js
+++ src/Companent/style.js
@@ -1,34 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    //  background-color: red;
      width: 100%;
      height: 700px;
-     /* border: 2px solid green; */
-    //  display: grid;
-    //  grid-template-columns: auto auto;
-    //  gap: 10px;
-    //  margin-left: 30px;
-    //  margin-top: 40px;
 `
-
-
 export const Boxs = styled.div`
-    //  background-color: green;
-    /* border: 2px solid red; */
      width: 910px;
      height: 700px;
      display: grid;
      grid-template-columns: auto auto;
      gap: 10px;
      margin-left: 50px;
-     /* background: red; */
-    //  margin-top: 40px;
 `
-
-
 export const Box1 = styled.div`
-    //  background-color: blue;
      border: 1px solid grey;
      width: 400px;
      height: 600px;
@@ -40,35 +24,26 @@ export const Box1 = styled.div`
 `
 
 export const Box2 = styled.div`
-    //  background-color: blue;
      border: 1px solid grey;
      width: 400px;
      height: 600px;
-    //  margin-left: 30px;
      margin-top: 40px;
      display: grid;
      align-items: center;
      justify-content: center;
 `
-
 export const Title = styled.a`
-     /* font-family: 'Inter'; */
      font-style: normal;
      margin-left: 50px;
      font-weight: 500;
      font-size: 20px;
      line-height: 16px;
-     /* align-items: center;
-     justify-content: center; */
      color: #3D3D3D;
-
      :hover{
         color: #46A358;
      }
 `
-
 export const P = styled.p`
-      /* border: 2px solid red; */
       font-style: normal;
       font-weight: 400;
       font-size: 13px;
@@ -77,21 +52,12 @@ export const P = styled.p`
 `
 
 export const Div = styled.div`
-      /* border: 2px solid blue; */
-      /* width: 100%; */
       height: 450px;
       padding-left: 100px;
-      /* display: grid; */
-      /* grid-template-columns: auto auto; */
-      /* justify-content: center; */
       align-items: center;
       margin-bottom: 90px;
-      /* position: relative;
-      bottom: 70px; */
 `
-
 export const Div1 = styled.div`
-      /* border: 2px solid red; */
       margin-top: 20px;
       margin-right: 50px;
 `
@@ -107,17 +73,13 @@ export const Button = styled.button`
       line-height: 16px;
       color: #FFFFFF;
 `
-
 export const P1 = styled.p`
-      /* border: 2px solid red; */
       margin-left: 85px;
       font-weight: 400;
       font-size: 13px;
       color: #3D3D3D;
 `
-
 export const Input = styled.input`
-    //  background-color: blue;
      margin: 7px 0px;
      border-radius: 5px;
      border: 1px solid grey;
@@ -129,12 +91,10 @@ export const Input = styled.input`
         line-height: 16px;
         text-align: start;
         color: #727272;
+        padding-left: 20px;
      }
-    //  margin-left: 30px;
-     /* margin-top: 40px; */
 `
 export const Input1 = styled.input`
-    //  background-color: blue;
      margin: 7px 0px;
      border-radius: 5px;
      border: 1px solid grey;
@@ -147,8 +107,10 @@ export const Input1 = styled.input`
         text-align: center;
         color: #727272;
      }
-    //  margin-left: 30px;
-     /* margin-top: 40px; */
 `
-
-// export {Container,Boxs, Box1, Box2, Title};
+export const P2 = styled.p`
+    font-weight: 400;
+    font-size: 14px;
+    color: #46A358;
+    margin-left: 170px;
+`
